ba43140b135664c29792867de436e359a32ef177
ohersi1 <osmanhersi@hotmail.co.uk>
2023-01-24 23:12:04 +0000

    added sticky navbar

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-const hamburgerIcon = document.querySelector('.hamburger-icon').querySelector('i');
-const close = document.querySelector('.close');
-const modal = document.querySelector('.modal');
-hamburgerIcon.addEventListener('click', function() {
-  modal.style.display = "grid";
-})
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const close = document.querySelector(".close");
+const modal = document.querySelector(".modal");
+const allLinks = document.querySelectorAll(".links2");
+const header = document.querySelector(".first-section");
+const secondSection = document.querySelector(".second-section");
+const navBar = document.querySelector(".nav-bar");
 
-close.addEventListener('click', function() {
-  modal.style.display = "none";
-})
+hamburgerIcon.addEventListener("click", function () {
+	modal.style.display = "grid";
+});
+
+function closeModal() {
+	modal.style.display = "none";
+}
+
+close.addEventListener("click", closeModal);
+
+function scrollIntoView(e) {
+	closeModal();
+	const block = e.target.dataset.block;
+	document.getElementById(block).scrollIntoView({ behavior: "smooth" });
+}
+allLinks.forEach((item) => {
+	item.addEventListener("click", scrollIntoView);
+});
+
+function stickyNav(entries) {
+	const [entry] = entries;
+	console.log(entry);
+	if (!entry.isIntersecting) {
+		navBar.classList.add("sticky");
+	} else navBar.classList.remove("sticky");
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+	root: null,
+	threshold: 0,
+});
+headerObserver.observe(header);
